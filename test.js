4899a7f00f3b3a4af213c40364d3a56573ad0459
iyasalowaneh <iyas_moh@yahoo.com>
2021-05-17 22:57:08 +0300

    just a trainig1

---
+++ test.js
@@ -0,0 +1,14 @@
+let firstName = "iyas";
+let midName = "mohammad";
+let lastName = "alowaneh";
+const age = 26
+let country = 'Jordan';
+let hoppy = "coding";
+let area = "amman";
+let favoritRoom = "bed room";
+let x = 5
+let y = 7
+
+if (x > y) {console.log(`my name is ${firstName} ${midName} ${lastName} and i'm ${age}. i'm from ${country}, i live in ${area}. i love ${hoppy} and my ${favoritRoom}`)}
+else if (x === y) {console.log(`its working, i love ${hoppy}`)}
+else {console.log("im not happy at all")}
